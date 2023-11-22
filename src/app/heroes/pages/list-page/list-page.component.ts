5cd4936920da0db1cf8e0eb14097da6c6d505e61
Joseph Escribano <joseph.escribano@worldline.com>
2023-11-22 19:35:53 +0100

    first commit

---
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesServiceService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-list-page',
+  templateUrl: './list-page.component.html',
+  styles: [
+  ]
+})
+export class ListPageComponent implements OnInit {
+
+
+  public heroes: Hero[] = [];
+
+
+
+  constructor(private heroesService: HeroesServiceService) {}
+
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+
+  }
+
+
+
+
+}
